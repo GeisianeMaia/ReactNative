5bc973d563a8d158ada0ef0e3b1acc3323cda6aa
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2020-07-30 23:04:26 -0300

    Card Home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,97 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { Searchbar } from 'react-native-paper'
+import React, { Fragment } from 'react';
+import { Text, View, Image, StyleSheet, ScrollView } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Home = () => {
   const [searchQuery, setSearchQuery] = React.useState('');
-
   const onChangeSearch = (query) => setSearchQuery(query);
 
   return (
-    <Searchbar
-      // style={styles.search}
-      placeholder="Search"
-      onChangeText={onChangeSearch}
-      value={searchQuery}
-    />
+    <Fragment>
+      <Searchbar
+        style={{ top: -50 }}
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+
+      <Text style={styles.textInfo}>Informações importantes</Text>
+
+      <View style={styles.info}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+          <TouchableOpacity style={styles.images}>
+            <Image
+              source={require('../home/img/info.jpg')}
+              style={styles.card}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.images}>
+            <Image
+              source={require('../home/img/info2.jpg')}
+              style={styles.card}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.images}>
+            <Image
+              source={require('../home/img/info3.jpg')}
+              style={styles.card}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.images}>
+            <Image
+              source={require('../home/img/info4.jpg')}
+              style={styles.card}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.images}>
+            <Image
+              source={require('../home/img/info5.jpg')}
+              style={styles.card}
+            />
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </Fragment>
   );
 };
 
-
 export default Home;
 
+const styles = StyleSheet.create({
+  textInfo: {
+    top: -20,
+    fontSize: 32,
+    color: '#35AAFF',
+  },
+  info: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+    
+  },
+  images: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 390,
+    width: 350,
+    borderRadius: 8,
+    paddingHorizontal: 2,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+  card: {
+    width: '100%',
+    height: '100%',
+  },
+});
